0dbd1952bc0e4e78e60f40ee7c6a6dbdc47fc372
chiabi <chiabi88@gmail.com>
2018-06-20 16:41:58 +0900

    Set defaultProps in user presentational components

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
 class UserForm extends Component {
-  state = {
-    username: '',
-    password: ''
+
+  static defaultProps = {
+    username: '', // user name 입력
+    password: '',  // user password 입력
+    onChangeInput: () => {} // 인풋 값 변경되면 실행되는 함수 
   }
+
   render() {
+    const {
+      username,
+      password,
+      onChangeInput
+    } = this.props;
     return (
       <React.Fragment>
-        <input type="text" value={this.state.username} placeholder="username" aria-label="username" />
-        <input type="password" password={this.state.password} placeholder="password" aria-label="password" />
+        <input type="text" value={username} onChange={onChangeInput} placeholder="username" aria-label="username" />
+        <input type="password" password={password} onChange={onChangeInput} placeholder="password" aria-label="password" />
       </React.Fragment>
     );
   }
--- src/components/UserFormButton.js
+++ src/components/UserFormButton.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
 class UserFormButton extends Component {
+
+  static defaultProps = {
+    onSubmitUser: () => {}, // 클릭하면 유저 정보를 보내는
+  }
+
   render() {
+    const {onSubmitUser} = this.props;
     return (
-      <button>Login</button>
+      <button onClick={onSubmitUser}>Login</button>
     );
   }
 }
