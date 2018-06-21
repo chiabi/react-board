f8910475029afa4303c7f5851a26aa23c0ac8a77
chiabi <chiabi88@gmail.com>
2018-06-21 17:22:07 +0900

    Solve storybook error with Router Add code from this link: [https://github.com/storybooks/storybook/issues/769]

--- src/components/BoardList.stories.js
+++ src/components/BoardList.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
@@ -24,5 +24,8 @@ const posts = [
   }
 ]
 storiesOf('BoardList', module)
+  .addDecorator(story => (
+    <BrowserRouter initialEntries={['/']}>{story()}</BrowserRouter>
+  ))
   .add('empty', () => <BoardList />)
-  .add('fulfilled', () => <BoardList posts={posts}/>)
+  .add('items', () => <BoardList posts={posts}/>)
--- src/components/BoardListItem.stories.js
+++ src/components/BoardListItem.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
@@ -7,11 +8,18 @@ import { linkTo } from '@storybook/addon-links';
 import BoardListItem from '../components/BoardListItem';
 
 const post = {
+  id: 0,
   title: 'Hello FDS JSON Server',
-  user: {
-    username: 'fds'
-  }
+  body: 'helloooooooooooo',
+  author: 'fds'
 }
 storiesOf('BoardListItem', module)
-  .add('empty', () => <BoardListItem />)
-  .add('fulfilled', () => <BoardListItem post={post}/>)
+  .addDecorator(story => (
+    <BrowserRouter initialEntries={['/']}>{story()}</BrowserRouter>
+  ))
+  .add('empty', () => (
+    <BoardListItem/>
+  ))
+  .add('fulfilled', () => (
+    <BoardListItem {...post}/>
+  ))
